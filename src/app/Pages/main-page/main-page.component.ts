62a0e4f0928c4d60d9f03f3aaee1817e8c0befa4
tomasch107 <tomasch107@gmail.com>
2020-05-03 15:40:22 +0200

    PZNU-1
    -aded main page

---
+++ src/app/Pages/main-page/main-page.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+declare var $: any;
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.scss']
+})
+export class MainPageComponent implements OnInit, AfterViewInit {
+
+  counted = false;
+  constructor() { }
+
+  ngOnInit(): void { }
+  ngAfterViewInit(): void {
+    var a = 0;
+    $(window).scroll(function() {
+      var oTop = $('#counter').offset().top - window.innerHeight;
+      if (a == 0 && $(window).scrollTop() > oTop) {
+        $('.counter-value').each(function() {
+          var $this = $(this),
+            countTo = $this.attr('data-count');
+          $({
+            countNum: $this.text()
+          }).animate({
+              countNum: countTo
+            },
+            {
+              duration: 2000,
+              easing: 'swing',
+              step: function() {
+                $this.text(Math.floor(this.countNum));
+              },
+              complete: function() {
+                $this.text(this.countNum);
+              }
+            });
+        });
+        a = 1;
+      }
+
+    });
+}
+
+}
+
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './Pages/main-page/main-page.component';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainPageComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
